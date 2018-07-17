eeb1c268dde8a0ff11cb6c1e84456e638c091c11
4everNoob <forever.noob@mail.com>
2018-07-16 22:29:52 -0700

    v.1.0

--- 16_power_digit_sum/README.md
+++ 16_power_digit_sum/README.md
@@ -1,4 +1,6 @@
-Power digit sum <br>
-Problem 16 <br>
-215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.<br>
-What is the sum of the digits of the number 2^1000?
+<h3>Power digit sum</h3>
+<h4>Problem 16</h4>
+<p>215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.</p>
+<p>What is the sum of the digits of the number 21000?</p>
+<p>v.1.0</p>
+<p>Used package big and big ints to perform all calculations.</p>
--- 16_power_digit_sum/main.go
+++ 16_power_digit_sum/main.go
@@ -6,38 +6,31 @@ import (
 )
 
 func main() {
-	//calculating 2^1000
+	// calculating 2^1000
 	power := new(big.Int).Exp(big.NewInt(2), big.NewInt(1000), nil)
 
-	//Initiating the sum of digits variable
+	// Initiating the sum of digits variable
 	sum := new(big.Int)
 
-	//Going through each digit while remainder is > 10
+	// Going through each digit while remainder is > 10
 	for power.Cmp(big.NewInt(10)) > 0 {
-		//power%10
+		// power%10
 		remainder := new(big.Int).Mod(power, big.NewInt(10))
-		//adding remainder to the sum variable
+		// adding remainder to the sum variable
 		sum = new(big.Int).Add(sum, remainder)
-		//separating remainder from the power to proceed with the next digit
+		// separating remainder from the power to proceed with the next digit
 		power = power.Div(power, big.NewInt(10))
 	}
-	//We are left with either 10 or number less than 10
+	// We are left with either 10 or number less than 10
 
-	//If we have 10 then just add 1 to the total sum (+0 and +1)
+	// If we have 10 then just add 1 to the total sum (+0 and +1)
 	if power.Cmp(big.NewInt(10)) == 0 {
 		sum = new(big.Int).Add(sum, big.NewInt(1))
-		//If we have any digit less than 10 then we add this digit to the total sum
+		// If we have any digit less than 10 then we add this digit to the total sum
 	} else {
 		sum = new(big.Int).Add(sum, power)
 	}
 
-	//Print result
+	// Print result
 	fmt.Println(sum)
 }
-
-/*
-Power digit sum 
-Problem 16 
-215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
-What is the sum of the digits of the number 2^1000?
-*/
